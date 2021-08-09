055f4cfcd629d572d492603e63077692bebf9ada
Ｒｅｘｉｎａｚｏｒ <85327329+Rexinazor@users.noreply.github.com>
2021-08-09 21:26:35 +0530

    Update index.php

--- Bot/index.php
+++ Bot/index.php
@@ -1,11 +1,5 @@
 <?php
-/*
-AUTHOR:- RITHUNAND [BENCHAMXD]
-CHANNEL:- @INDUSBOTS 
-THIS REPO IS LICENCED WITH GENERAL PUBLIC LICENSE VERSION:3.0
 
-(c) RITHUNAND K
-*/
 
 require_once __DIR__ . "/config.php";
 
@@ -68,15 +62,15 @@ Alvi('sendMessage',[
 
 I'm $BOT_NAME a powerfull AI Bot
 
-I will resopnd to any image you send. Send me a image to me, I will say what is That😜
+I will resopnd to any image you send. Send me a image and I will say what is it! It would be really funny xD....
 
-Also add me to your group and make me admin. I'll reply to every photo😌.***",
+Also add me to your group and make me admin. I'll reply to every photo.***",
 'reply_to_message_id'=>$message_id,
 'parse_mode'=>"MarkDown",
 'reply_markup' =>  json_encode([
 'inline_keyboard' => [
-[['text' => "SUPPORT",'url' => "https://telegram.me/induschats"],['text' => "CREATOR", 'url' => "https://telegram.me/benchamxd"]],
-[['text' => "ADD ME", 'url' => "https://telegram.me/$BOT_USERNAME?startgroup=False"],['text' => "Repo", 'url' => "https://github.com/Benchamxd/Ai-Bot"]], 
+[['text' => "SUPPORT",'url' => "https://telegram.me/BHZBot_Support"],['text' => "UPDATES", 'url' => "https://telegram.me/BioHazard_Bots"]],
+[['text' => "ADD ME", 'url' => "https://telegram.me/$BOT_USERNAME?startgroup=False"],['text' => "Creator", 'url' => "https://telegram.me/Avrazel"]], 
 ]])
 ]);
 }
